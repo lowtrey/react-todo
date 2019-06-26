7ef46f44c133dee4b2262606b00807efefb0ea51
Q. Price <priceqp@gmail.com>
2019-06-26 18:23:08 -0500

    Add ability to edit todos

--- src/TodoList.js
+++ src/TodoList.js
@@ -18,6 +18,7 @@ class TodoList extends React.Component {
         ]};
         this.addTask = this.addTask.bind(this);
         this.remove = this.remove.bind(this);
+        this.update = this.update.bind(this);
     }
     addTask(newTodo) {
         this.setState({
@@ -29,13 +30,23 @@ class TodoList extends React.Component {
             todos: this.state.todos.filter(todo => todo.id !== id)
         });
     }
+    update(id, updatedTask) {
+        const updatedTodos = this.state.todos.map(todo => {
+            if(todo.id === id){
+                return {...todo, task: updatedTask}
+            }
+            return todo;
+        });
+        this.setState({todos: updatedTodos});
+    }
     render() {
         const todos = this.state.todos.map(todo => (
             <Todo
                 key={todo.id} 
                 id={todo.id} 
                 task={todo.task} 
-                remove={this.remove} 
+                remove={this.remove}
+                updateTodo={this.update} 
             />
         ));
         return (
